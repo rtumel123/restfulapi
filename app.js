86a0498cba4d9c5a3c929ad9f1b4b769cd29a4a3
Ryan Tumel <rtumel123@gmail.com>
2020-12-01 12:39:18 -0500

    connect mongoDB

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
 
 // ROUTES
 app.get('/', (req, res) => {
   res.send('On Home');
 });
+
+app.get('/posts', (req, res) => {
+  res.send('On posts');
+});
+
+// CONNECT TO DB
+mongoose.connect('mongodb+srv://test:r123@rest.gyput.mongodb.net/REST?retryWrites=true&w=majority',
+ { useNewUrlParser: true }, 
+ () => console.log('connected to db')
+);
 
 app.listen(3000);
