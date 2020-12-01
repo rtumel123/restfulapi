2f54fec208ef1accdbb16de75283aec8ba6b20ab
Ryan Tumel <rtumel123@gmail.com>
2020-12-01 13:41:11 -0500

    add specific post route

--- routes/posts.js
+++ routes/posts.js
@@ -3,16 +3,18 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
+// GET ALL POSTS
 router.get('/', async (req, res) => {
   try {
     // return posts with mongoose
     const posts = await Post.find();
     res.json(posts);
-  } catch(err) {
+  } catch (err) {
     res.json({ message: err })
   }
 });
 
+// SUBMITS A POST
 router.post('/', async (req, res) => {
   const post = new Post({
     title: req.body.title,
@@ -21,6 +23,16 @@ router.post('/', async (req, res) => {
   try {
     const savedPost = await post.save()
     res.json(savedPost)
+  } catch (err) {
+    res.json({ message: err })
+  };
+});
+
+// GET SPECIFIC POST
+router.get('/:postId', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postId);
+    res.json(post);
   } catch(err) {
     res.json({ message: err })
   };
