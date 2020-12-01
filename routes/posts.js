5725f152bbc2396f686094a095e375b9e15acde9
Ryan Tumel <rtumel123@gmail.com>
2020-12-01 13:36:06 -0500

    finish get and post

--- routes/posts.js
+++ routes/posts.js
@@ -3,8 +3,14 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-  res.send('On posts');
+router.get('/', async (req, res) => {
+  try {
+    // return posts with mongoose
+    const posts = await Post.find();
+    res.json(posts);
+  } catch(err) {
+    res.json({ message: err })
+  }
 });
 
 router.post('/', async (req, res) => {
